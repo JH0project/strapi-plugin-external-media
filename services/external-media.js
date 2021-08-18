5f5238b47c02ed5e292a9e1b4c6e24563d9d00cb
Jonathan Ho <heiheiho000@gmail.com>
2021-08-18 19:49:51 +0800

    build: run strapi generate:plugin

---
+++ services/external-media.js
@@ -0,0 +1,11 @@
+'use strict';
+
+/**
+ * external-media.js service
+ *
+ * @description: A set of functions similar to controller's actions to avoid code duplication.
+ */
+
+module.exports = {
+
+};
